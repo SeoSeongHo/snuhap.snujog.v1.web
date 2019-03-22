48002c151f64798a21eadb7f19aa9d9b0a35157e
carter-igaw <carter.seo@igaworks.com>
2019-03-22 10:18:07 +0900

    initial commit

---
+++ apps/snuhap.snujog.v1.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace snuhap.snujog.v1.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
